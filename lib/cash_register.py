8a5ac0aabdf38038a767ce0b3e19b1add9251ff4
mark9559 <mwangimark57@gmail.com>
2023-12-01 14:54:10 +0300

    Corrected Completed Assignment

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,34 +1,30 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-    def __init__(self, discount=0):
-        self.discount = discount
-        self.total = 0
-        self.items = []
+  def __init__(self, discount=0):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+    self.last_item = {"title": "", "price": 0, "quantity": 1}
 
-    def add_item(self, title, price, quantity=1):
-        self.total += price * quantity
-        self.items.extend([title] * quantity)
+  def add_item(self, title, price, quantity=1):
+    self.last_item["title"] = title
+    self.last_item["price"] = price
+    self.last_item["quantity"] = quantity
+    i = quantity
+    while i >= 1:
+      self.items.append(title)
+      i -= 1
 
-    def apply_discount(self):
-        if self.discount > 0:
-            self.total -= (self.total * self.discount) // 100
-            print(f"After the discount, the total comes to ${self.total}.")
-        else:
-            print("There is no discount to apply.")
+    total_price = price * quantity
+    self.total = self.total + total_price 
 
-    def void_last_transaction(self):
-        if self.items:
-            last_item_price = self.items.pop()
-            self.total -= last_item_price
+  def apply_discount(self):
+    if self.discount != 0:
+      self.total = self.total - (self.discount * 0.01 * self.total)
+      print(f"After the discount, the total comes to ${int(self.total)}.")
+    else:
+      print("There is no discount to apply.")
 
-    def reset_register_totals(self):
-        self.total = 0
-        self.items = []
-
-    def test_void_last_transaction_with_multiples(self):
-        self.cash_register.add_item("tomato", 1.76, 2)
-        self.cash_register.void_last_transaction() 
-        assert(self.cash_register.total == 1.76)
-        self.cash_register.void_last_transaction()
-        assert(self.cash_register.total == 0.0)
+  def void_last_transaction(self):
+    self.total = self.total - (self.last_item["price"] * self.last_item["quantity"])
